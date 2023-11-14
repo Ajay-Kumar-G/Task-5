75c9cc082c7de41518d849720daefab94bbd889b
Aj_AI <148686027+Ajay-Kumar-G@users.noreply.github.com>
2023-11-14 17:32:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// This method is used to print the message inside the double quotes
+console.log("Hey ! Welcome to Javascript");
+
+// this method is to get alert notifications while we open a browser
+window.alert("Welcome Buddy");
+
+// here the name-"Ajay", is assigned to variable n1 and printed
+let n1="Ajay";
+console.log(n1);
+
+// using camel-case condition
+let rollNumber=17;
+console.log(rollNumber);
+
+// using const keyword
+const n=123;
+console.log(n);
+
+
+// Performing arithematic operations
+// taking the input from the user and assigning it to 'a' and 'b'
+a=parseInt(prompt("Enter the nuber1: "));
+b=parseInt(prompt("Enter the second number: "));
+// addition operation
+c=a+b;
+console.log(c);
+// substraction operation
+d=a-b;
+console.log(d);
+// multiplication
+e=a*b;
+console.log(e);
+// Division
+f=a/b;
+console.log(f);
+// Modulo operation
+g=a%b;
+console.log(g);
